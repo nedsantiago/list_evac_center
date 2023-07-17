1a244fa919d6309ae9c1fb75d3756e5c1f864f7d
Ned Santiago <nedsantiago@tutanota.com>
2023-07-17 10:28:27 +0800

    Attempted to apply Object Oriented Programming into the python app. Nearly the entire list processor is now encapsulated inside a single class called evac_list_processor. The processor can be easily imported into another file with a single class call.

--- process_list.py
+++ process_list.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import re
+from re import search, sub
 
 COL_NAME = "NAME OF EVACUATION CENTER"
 COL_ADDR = "ADDRESS"
@@ -13,129 +13,110 @@ OUTPUT_FILE_NAME = "new_list.csv"
 def main():
     # import the file to be used
     file_dir = "list_evac_center_garie.txt"
-    with open(file_dir, "r", encoding="utf-8") as file:
-        reader = file.readlines()
-        # initialize an empty list where dictionaries of each row will be placed
-        list_of_dict = []
-        # iterate over each row of the csv file
-        for row in reader:
-            # some of the rows have no length (empty), try skips those
-            if row != ""  or row != "\n":
-                print(f"{repr(row)}")
-                # initialize the string as empty
-                string = ""
-                # initialize the dictionary to be empty
-                dictionary = dict.fromkeys([COL_NAME, COL_ADDR, COL_CAP, COL_LAT, COL_LONG])
-
-                # flag whether a value was added to the dicitonary
-                has_value = False
-                # iterate through each character checking for conditions
-                for i in range(len(row)):
-                    BUFFER = 5
-                    string += row[i]
-                    string_buffer = row[i+1:i+BUFFER+1]
-                    # check if next 5 characters in string is either "Brgy" or "Bray"
-                    if is_brgy_bray(string_buffer):
-                        # append the string to a dictionary as "NAME OF EVACUATION CENTER"
-                        dictionary[COL_NAME] = string.strip()
-                        has_value = True
-                        string = ""
-                    # check if next 5 characters in string is either " ## " or "### "
-                    elif is_capacity_next(string_buffer):
-                        # append the string to a dicitonary as "ADDRESS"
-                        dictionary[COL_ADDR] = string.strip()
-                        has_value = True
-                        string = ""
-                    # check if next 5 characters in string is "##.##"
-                    elif is_latitude_next(string_buffer):
-                        print(f"Capacity: {string}, activated by {string_buffer}")
-                        # append the string to a dictionary as "INDIVIDUAL CAPACITY"
-                        dictionary[COL_CAP] = string.strip()
-                        has_value = True
-                        string = ""
-                    # check if next 5 characters in string is "###.#"
-                    elif is_longitude_next(string):
-                        # append the string to a dictionary as "LATITUDE"
-                        dictionary[COL_LAT] = string.strip()
-                        print(f"Latitude: {dictionary[COL_LAT]}, activated by {repr(string_buffer)}")
-                        has_value = True
-                        string = ""
-                    # append the remaining string to a dictionary as "LONGITUDE"
-                    elif (dictionary[COL_LAT] != None):
-                        dictionary[COL_LONG] = string.strip()
-                        print(f"Longitude:\"{dictionary[COL_LONG]}\"")
-                # append dictionary to list if not empty
-                if has_value:
-                    list_of_dict.append(dictionary)
-
-        df = pd.DataFrame(list_of_dict)
-    print(list_of_dict)
-    print(df[[COL_NAME,COL_LAT,COL_LONG]].head(3))
-    print(df[[COL_LAT,COL_LONG]].tail(3))
-    df.to_csv(OUTPUT_FILE_NAME)
-    # write_df_to_csv(df=df, file_name=FILE_NAME[:-4], iteration=0)
+    processor = evac_list_processor()
+    processor.process(file_dir)
+    processor.df.to_csv("new_list.csv")
 
 class evac_list_processor():
     """This class encompasses all the functionality for converting
     a text document into a dataframe and exporting that"""
 
-    def __init__(self) -> None:
-        pass
+    def process(self, file_dir) -> None:
+        with open(file_dir, "r", encoding="utf-8") as file:
+            reader = file.readlines()
+            # initialize an empty list where dictionaries of each row will be placed
+            list_of_dict = []
+            # iterate over each row of the csv file
+            for row in reader:
+                # some of the rows have no length (empty), try skips those
+                if row != ""  or row != "\n":
+                    # initialize the string as empty
+                    string = ""
+                    # initialize the dictionary to be empty
+                    dictionary = dict.fromkeys([COL_NAME, COL_ADDR, COL_CAP, COL_LAT, COL_LONG])
 
-def is_brgy_bray(string:str)-> bool:
-    """check characters in string is either "Brgy" or "Bray" """
-    REGEX = r"Br[ga]y."
-    return re.search(REGEX, string) != None
+                    # flag whether a value was added to the dicitonary
+                    has_value = False
+                    # iterate through each character checking for conditions
+                    for i in range(len(row)):
+                        BUFFER = 5
+                        string += row[i]
+                        string_buffer = row[i+1:i+BUFFER+1]
+                        # check if next 5 characters in string is either "Brgy" or "Bray"
+                        if self.is_brgy_bray(string_buffer):
+                            # append the string to a dictionary as "NAME OF EVACUATION CENTER"
+                            dictionary[COL_NAME] = string.strip()
+                            has_value = True
+                            string = ""
+                        # check if next 5 characters in string is either " ## " or "### "
+                        elif self.is_capacity_next(string_buffer):
+                            # append the string to a dicitonary as "ADDRESS"
+                            dictionary[COL_ADDR] = string.strip()
+                            has_value = True
+                            string = ""
+                        # check if next 5 characters in string is "##.##"
+                        elif self.is_latitude_next(string_buffer):
+                            # append the string to a dictionary as "INDIVIDUAL CAPACITY"
+                            dictionary[COL_CAP] = string.strip()
+                            has_value = True
+                            string = ""
+                        # check if next 5 characters in string is "###.#"
+                        elif self.is_longitude_next(string_buffer):
+                            # append the string to a dictionary as "LATITUDE"
+                            string = self.clean_number(string=string)
+                            dictionary[COL_LAT] = string.strip()
+                            has_value = True
+                            string = ""
+                        # append the remaining string to a dictionary as "LONGITUDE"
+                        elif (dictionary[COL_LAT] != None):
+                            string = self.clean_number(string=string)
+                            dictionary[COL_LONG] = string.strip()
+                    # append dictionary to list if not empty
+                    if has_value:
+                        list_of_dict.append(dictionary)
 
-def is_capacity_next(string:str)-> bool:
-    """check characters in string is either " ## " or "### " """
-    REGEX1 = r"[^\S\r\n][0-9]{2}[^\S\r\n]."
-    REGEX2 = r"[^\S\r\n][0-9]{3}[^\S\r\n]"
-    return (re.search(REGEX1, string) != None) or (re.search(REGEX2, string) != None)
+            df = pd.DataFrame(list_of_dict)
+            self.df = df
+        print(list_of_dict)
+        print(df[[COL_NAME,COL_LAT,COL_LONG]].head(3))
+        print(df[[COL_LAT,COL_LONG]].tail(3))
 
-def is_latitude_next(string:str)-> bool:
-    """check if characters in string is "##.##" """
-    REGEX = r"15[.,][0-9]{2}"
-    return re.search(REGEX, string) != None
+    def is_brgy_bray(self, string:str)-> bool:
+        """check characters in string is either "Brgy" or "Bray" """
+        REGEX = r"Br[ga]y."
+        return search(REGEX, string) != None
 
-def is_longitude_next(string:str)-> bool:
-    """check if characters in string is "###.#" """
-    REGEX = r"120[.,][0-9]"
-    REGEX_EXCEPT = r"^[\n]"
-    return (re.search(REGEX, string) != None) and (re.search(REGEX_EXCEPT, string) == None)
-    
-def is_at_end(string:str)-> bool:
-    """check if it is the last character for the row \n """
-    REGEX = r"[0-9]{4}[\n]"
-    return re.search(REGEX, string) != None
+    def is_capacity_next(self, string:str)-> bool:
+        """check characters in string is either " ## " or "### " """
+        REGEX1 = r"[^\S\r\n][0-9]{2}[^\S\r\n]."
+        REGEX2 = r"[^\S\r\n][0-9]{3}[^\S\r\n]"
+        return (search(REGEX1, string) != None) or (search(REGEX2, string) != None)
 
-def clean_number(string:str)-> str:
-    """this function takes a string of numbers, and removes any
-    characters that are neither numeric nor a period while converting
-    commas into periods"""
-    pass
+    def is_latitude_next(self, string:str)-> bool:
+        """check if characters in string is "15.##" """
+        REGEX = r"15[.,][0-9]{2}"
+        return search(REGEX, string) != None
 
-def write_df_to_csv(df:pd.DataFrame, file_name:str, iteration:int)-> None:
-    """This function attempts to use the to_csv method of a dataframe.
-    If an exception is raised, that might mean that there is an existing
-    file in use under that name, then the function attempts to write the
-    file to modify the name until it either reaches its limit or it does
-    not encounter an error."""
-    
-    # set the limitation on the iteration
-    ITER_LIMIT = 5
-    
-    # try the to_csv method of the dataframe
-    try:
-        df.to_csv(file_name + ".csv")
-    except:
-        if iteration < 5:
-            iteration += 1
-            print(iteration)
-            write_df_to_csv(df, file_name, iteration)
-        else:
-            pass
+    def is_longitude_next(self, string:str)-> bool:
+        """check if characters in string is "120.#" """
+        REGEX = r"120[.,][0-9]"
+        return search(REGEX, string) != None
+        
+    def is_at_end(self, string:str)-> bool:
+        """check if it is the last character for the row \n """
+        REGEX = r"[0-9]{4}[\n]"
+        return search(REGEX, string) != None
 
+    def clean_number(self, string:str)-> str:
+        """this function takes a string of numbers, and removes any
+        characters that are neither numeric nor a period while converting
+        commas into periods"""
+
+        REGEX1 = r"[|]"
+        string = sub(REGEX1, r"", string=string)
+        REGEX2 = r","
+        string = sub(REGEX2, r".", string=string)
+        return string
 
 if __name__ == "__main__":
     main()
--- test_process_list.py
+++ test_process_list.py
@@ -3,40 +3,46 @@ import process_list
 
 def main():
     print("testing test_process_list.py")
+    test_latitude_next()
+    test_longitude_next()
+    test_first_two_data_rows()
     pass
 
 def test_latitude_next():
     """is_latitiude_next should only trigger when a number starts with 15 """
-    assert process_list.is_latitude_next("15.34") == True
-    assert process_list.is_latitude_next("15,34") == True
-    assert process_list.is_latitude_next("15345") == False
-    assert process_list.is_latitude_next("11.245") == False
-    assert process_list.is_latitude_next("abcde") == False
+    processor = process_list.evac_list_processor()
+    assert processor.is_latitude_next("15.34") == True
+    assert processor.is_latitude_next("15,34") == True
+    assert processor.is_latitude_next("15345") == False
+    assert processor.is_latitude_next("11.245") == False
+    assert processor.is_latitude_next("abcde") == False
 
 def test_longitude_next():
     """is_longitude_next should only return true when starts with 120"""
-    assert process_list.is_longitude_next("120.4") == True
-    assert process_list.is_longitude_next("120,4") == True
-    assert process_list.is_longitude_next("12023") == False
-    assert process_list.is_longitude_next("11.24") == False
-    assert process_list.is_longitude_next("abcde") == False
-    assert process_list.is_longitude_next("11815") == False
-    assert process_list.is_longitude_next("139\n") == False
-    assert process_list.is_longitude_next("139\r") == False
-    assert process_list.is_longitude_next("167 |") == False
+
+    processor = process_list.evac_list_processor()
+    assert processor.is_longitude_next("120.4") == True
+    assert processor.is_longitude_next("120,4") == True
+    assert processor.is_longitude_next("12023") == False
+    assert processor.is_longitude_next("11.24") == False
+    assert processor.is_longitude_next("abcde") == False
+    assert processor.is_longitude_next("11815") == False
+    assert processor.is_longitude_next("139\n") == False
+    assert processor.is_longitude_next("139\r") == False
+    assert processor.is_longitude_next("167 |") == False
 
 def test_first_two_data_rows():
     """this function takes the first two dataliens of the original text
     file, because these lines seem to illustrate an odd interaction seen
     throughout the text file whereby \n seems to be read as a number"""
+
+    processor = process_list.evac_list_processor()
     
     file_name = r"./test_data/first_two_data_rows.txt"
-    with open(file_name, "r", encoding="utf-8") as file:
-        reader = file.readlines()
-        for row in reader:
-            pass
-    pass
-
+    processor.process(file_name)
+    print("\noutput dataframe")
+    assert processor.df["LONGITUDE"].iloc(0)[0] == "120.4139"
+    assert processor.df["LATITUDE"].iloc(0)[0] == "15.68754167"
     
 
 if __name__ == "__main__":
